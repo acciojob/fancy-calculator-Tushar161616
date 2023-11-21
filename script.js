53b817e99c9395e534c1c18b6be67612dad44967
Tushar Sharma <145897973+Tushar161616@users.noreply.github.com>
2023-11-21 16:35:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const mul = (let val1, leval2) => {
+	let sum = val1*2;
+	let ans = sum + val2;
+	return ans;
+}
+const val1 = prompt("Enter value 1");
+const val2 = prompt("Enter value 2");
+alert(mul(val1,val2));
